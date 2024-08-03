6ba084433088dcec54ef86fce4b80432451ea403
yemi02 <yemi.adetola.ya@gmail.com>
2024-08-04 00:59:31 +0100

    navbar completed

--- frontend/src/pages/Home/HomeScreen.jsx
+++ frontend/src/pages/Home/HomeScreen.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { useAuthStore } from "../../store/authUser";
+import Navbar from "../../components/Navbar";
 
 const HomeScreen = () => {
-  const { logout } = useAuthStore();
   return (
-    <div>
-      HomeScreen
-      <button onClick={logout}>logout</button>
-    </div>
+    <>
+      <div className="relative h-screen text-white bg-black">
+        <Navbar />
+      </div>
+    </>
   );
 };
 

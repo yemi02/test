32c82451934d449d4c708a2fdd23a731e3e70f06
yemi02 <yemi.adetola.ya@gmail.com>
2024-08-05 14:31:31 +0100

    home page completed

---
+++ frontend/src/components/MovieSlider.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useRef, useState } from "react";
+import { useContentStore } from "../store/content";
+import { Link } from "react-router-dom";
+import { SMALL_IMG_BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+const MovieSlider = ({ category }) => {
+  const { contentType } = useContentStore();
+  const [content, setContent] = useState([]);
+  const [showArrows, setShowArrows] = useState(false);
+
+  const sliderRef = useRef(null);
+
+  const formattedCategoryName =
+    category[0].toUpperCase() + category.replaceAll("_", " ").slice(1);
+
+  const formattedContentType = contentType === "movie" ? "Movie" : "TV Shows";
+
+  useEffect(() => {
+    const getContent = async () => {
+      const res = await axios.get(`/api/v2/${contentType}/${category}`);
+      setContent(res.data.content);
+    };
+    getContent();
+  }, []);
+
+  const scrollLeft = () => {
+    console.log("left");
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.offsetwidth,
+        behavior: "smooth",
+      });
+    }
+  };
+  const scrollRight = () => {
+    console.log("right");
+
+    sliderRef.current.scrollBy({
+      left: sliderRef.current.offsetwidth,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div
+      className="relative bg-black text-white px-5 md:px-20 "
+      onMouseEnter={() => setShowArrows(true)}
+      onMouseLeave={() => setShowArrows(false)}
+    >
+      <h2 className="mb-4 text-2xl font-bold">
+        {formattedCategoryName} {formattedContentType}
+      </h2>
+
+      <div
+        className="flex space-x-4 overflow-x-scroll scrollbar-hide"
+        ref={sliderRef}
+      >
+        {content.map((item) => (
+          <Link
+            to={`/watch/${item.id}`}
+            key={item.id}
+            className="min-w-[250px] relative group"
+          >
+            <div className="rounded-lg overflow-hidden">
+              <img
+                src={SMALL_IMG_BASE_URL + item.backdrop_path}
+                alt="Movie image"
+                className="transition-transform duration-200 ease-in-out group-hover:scale-125"
+              />
+            </div>
+            <p className="mt-2 text-center">{item.title || item.name}</p>
+          </Link>
+        ))}
+      </div>
+      {showArrows && (
+        <>
+          <button
+            className="absolute top-1/2 -translate-y-1/2 left-5 md:left-24 flex items-center justify-center size-8 rounded-full bg-black bg-opacity-60 hover:bg-opacity-80 text-white z-10 "
+            onClick={scrollLeft}
+          >
+            <ChevronLeft size={24} />
+          </button>
+          <button
+            className="absolute top-1/2 -translate-y-1/2 right-5 md:right-24 flex items-center justify-center size-8 rounded-full bg-black bg-opacity-60 hover:bg-opacity-80 text-white z-10 "
+            onClick={scrollRight}
+          >
+            <ChevronRight size={24} />
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MovieSlider;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,18 @@ import { LogOut, Menu, Search } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/authUser";
+import { useContentStore } from "../store/content";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const { user, logout } = useAuthStore();
 
   const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+
+  const { setContentType } = useContentStore();
+
   return (
-    <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20 ">
+    <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
       <div className="flex items-center gap-10 z-50">
         <Link to={"/"}>
           <img
@@ -21,10 +25,18 @@ const Navbar = () => {
 
         {/* desktop navbar items */}
         <div className="hidden sm:flex gap-2 items-center">
-          <Link to={"/"} className="hover:underline">
+          <Link
+            to={"/"}
+            className="hover:underline"
+            onClick={() => setContentType("movie")}
+          >
             Movies
           </Link>
-          <Link to={"/"} className="hover:underline">
+          <Link
+            to={"/"}
+            className="hover:underline"
+            onClick={() => setContentType("tv")}
+          >
             Tv Shows
           </Link>
           <Link to={"/history"} className="hover:underline">
@@ -68,7 +80,7 @@ const Navbar = () => {
             Tv Shows
           </Link>
           <Link
-            to={"/"}
+            to={"/history"}
             className="block hover:underline p-2"
             onClick={toggleMobileMenu}
           >

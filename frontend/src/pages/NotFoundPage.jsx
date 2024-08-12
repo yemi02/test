86d5ae4065485502b87a0086ba12450757c8c211
yemi02 <yemi.adetola.ya@gmail.com>
2024-08-12 20:37:36 +0100

    not found page bug fix

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
   return (
     <div
-      className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
+      className="h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
       style={{ backgroundImage: `url('/404.png')` }}
     >
       <header className="absolute top-0 left-0 p-4 bg-black w-full ">
